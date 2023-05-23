3767d167d6ee749c67f669e664c9ec8a238ba479
pjione <a41924250@gmail.com>
2023-05-23 23:26:07 +0900

    Initial commit

---
+++ src/main/java/com/example/typeconverter/converter/StringToIntergerConverter.java
@@ -0,0 +1,11 @@
+package com.example.typeconverter.converter;
+
+
+import org.springframework.core.convert.converter.Converter;
+
+public class StringToIntergerConverter implements Converter<String, Integer> {
+    @Override
+    public Integer convert(String source) {
+        return null;
+    }
+}
